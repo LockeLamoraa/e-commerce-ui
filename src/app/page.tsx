d771aa0fae0bc90b8d97d3d2174ce22575ff7fab
jovannie <jovanniedelosreyes@gmail.com>
2025-10-23 11:46:07 +0800

    Site Navbar and Footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image"
+
 const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
+    return (
+        <div className='relative aspect-[3/1] mb-12'>
+            <Image src="/featured.png" alt="Featured Product" fill />
+        </div>
+    )
 }
 
 export default Homepage
